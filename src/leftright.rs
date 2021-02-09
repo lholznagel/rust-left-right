f110c374b716ad0e5ba357a96883d37f195a4152
lholznagel <contact@lholznagel.info>
2021-02-09 14:35:14 +0100

    Optimized leftright

--- src/leftright.rs
+++ src/leftright.rs
@@ -17,13 +17,15 @@ impl<T: Clone> LeftRight<T> {
     pub fn read<F, R>(&self, f: F) -> R
     where F: Fn(&T) -> R {
 
-        self.arrive();
-        let result = if self.is_left_read() {
+        let is_left = self.arrive();
+
+        // depending on which side is active, give it the reader
+        let result = if is_left {
             unsafe { f(& (*self.left.get())) }
         } else {
             unsafe { f(& (*self.right.get())) }
         };
-        self.depart();
+        self.depart(is_left);
         result
     }
 
@@ -31,21 +33,27 @@ impl<T: Clone> LeftRight<T> {
     where F: Fn(&mut T) {
 
         let is_left_read = self.is_left_read();
+
+        // depending on which side is activ, change the not active side
         if !is_left_read {
             unsafe { f(&mut (*self.left.get()))}
         } else {
             unsafe { f(&mut (*self.right.get()))}
         }
 
-        self.switch();
-    }
-
-    fn switch(&self) {
-        let is_left_read = self.is_left_read();
+        // switch the reader from left to right or from right to left
         self.leftright.store(!is_left_read, Ordering::SeqCst);
 
-        while self.has_old_readers(!is_left_read) { }
+        // wait until all readers switched to the new side
+        loop {
+            if is_left_read {
+                if self.readers_left.load(Ordering::SeqCst) > 0 { continue; } else { break; }
+            } else {
+                if self.readers_right.load(Ordering::SeqCst) > 0 { continue; } else { break; }
+            }
+        }
 
+        // finally copy the new data over to the other side
         if is_left_read {
             unsafe { *(self.left.get() as *mut _) = &mut *self.right.get() };
         } else {
@@ -53,27 +61,31 @@ impl<T: Clone> LeftRight<T> {
         }
     }
 
-    fn arrive(&self) {
+    /// Depending on the active side, it increments the counter
+    ///
+    /// The active side is returned
+    /// true -> left
+    /// false -> right
+    fn arrive(&self) -> bool {
         if self.is_left_read() {
             self.readers_left.fetch_add(1, Ordering::SeqCst);
+            true
         } else {
             self.readers_right.fetch_add(1, Ordering::SeqCst);
+            false
         }
     }
 
-    fn depart(&self) {
-        if self.is_left_read() {
+    /// Decrements the counter for a side depending on the given bool.
+    ///
+    /// `is_left`
+    /// true -> left
+    /// false -> right
+    fn depart(&self, is_left: bool) {
+        if is_left {
             self.readers_left.fetch_sub(1, Ordering::SeqCst);
         } else {
             self.readers_right.fetch_sub(1, Ordering::SeqCst);
-        }
-    }
-
-    fn has_old_readers(&self, current_reader: bool) -> bool {
-        if !current_reader {
-            self.readers_left.load(Ordering::SeqCst) > 0
-        } else {
-            self.readers_right.load(Ordering::SeqCst) > 0
         }
     }
 
